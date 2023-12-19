7d6fcca1a7fcb473d18de0e691928d21ccefde96
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2023-12-19 23:17:49 +0700

    tp04

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+use Str;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('products')->insert([
+            'name' => Str::random(10),
+            'category_id' => 1,
+            'pricing' => 10.99,
+            'description' => "This is the description.",
+        ]);
+    }
+}
